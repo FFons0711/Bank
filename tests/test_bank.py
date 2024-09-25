8dda372034da29e5ce4bda744a152cc264b6f6a2
FFon0711 <ff010711@gmail.com>
2024-09-24 23:37:18 -0400

    Some minor change to test_bank

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -1,5 +1,5 @@
 import unittest
-from main import Client
+from main import Client, validate_card, generate_number, compound_interest
 
 
 class ClientTests(unittest.TestCase):
@@ -16,3 +16,23 @@ class ClientTests(unittest.TestCase):
         with self.assertRaises(NameError):
             self.account.withdraw(80000)
 
+
+def test_validate_card():
+    assert validate_card("1249760291234567") == True
+    assert validate_card("1234567891234567") == False
+    assert validate_card("12497602912345678") == False
+    assert validate_card("12497602912345") == False
+    assert validate_card("12497602cat") == False
+    assert validate_card("cat") == False
+
+
+def test_generate_number():
+    number = generate_number()
+    assert len(number) == 16
+
+
+def test_compound_interest():
+    n, y1, y2 = compound_interest(1000, 0.1, 4, 10)
+    assert n == 2685.06
+    assert y1 == [1103.81, 1218.4, 1344.89, 1484.51, 1638.62, 1808.73, 1996.5, 2203.76, 2432.54, 2685.06]
+    assert y2 == [1000.0, 1100.0, 1200.0, 1300.0, 1400.0, 1500.0, 1600.0, 1700.0, 1800.0, 1900.0]
